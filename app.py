ecf9336b4b704213c96ee65f4a067d5bf7594f76
verdantfire <himanshuruhela013@gmail.com>
2021-04-22 01:43:47 +0530

    Improvements to comparison page

--- app.py
+++ app.py
@@ -56,14 +56,14 @@ def result():
 @app.route('/compare', methods=['GET','POST'])
 def compare():
     global results
-    results_avg = {"Wellness score":51.931927366908305, "Personal Wellness Score": 42.85714285714247,
-            "Workplace Wellness Score": 71.53872985658633,
-            "Subjective Wellness Score": 48.88504090599943,
-            "Physical Wellness Score": 88.85961065464072,
-            "Social Wellness Score": 97.3460764539568,
-            "Employee Satisfaction": 77.81350482315112,
-            "Environment Satisfaction": 69.29260450160771,
-            "Work Life Balance": 68.2475884244373}
+    results_avg = {"Wellness score":51.932, "Personal Wellness Score": 42.857,
+            "Workplace Wellness Score": 71.539,
+            "Subjective Wellness Score": 48.885,
+            "Physical Wellness Score": 88.860,
+            "Social Wellness Score": 97.346,
+            "Employee Satisfaction": 77.814,
+            "Environment Satisfaction": 69.293,
+            "Work Life Balance": 68.248}
 
     return render_template('compare.html', employee_name = results["Employee Name"], Age_Group = results["Age"], Gender = results["Gender"], wellness_score = results["Wellness score"],personal_wellness_score = results["Personal Wellness Score"],workplace_wellness_score = results["Workplace Wellness Score"],subjective_wellness_score = results["Subjective Wellness Score"],physical = results["Physical Wellness Score"],social = results["Social Wellness Score"] ,emp_satifaction = results["Employee Satisfaction"],env_satisfaction = results["Environment Satisfaction"],work_life_balance = results["Work Life Balance"], wellness_score_avg = results_avg["Wellness score"], personal_wellness_score_avg = results_avg["Personal Wellness Score"],workplace_wellness_score_avg = results_avg["Workplace Wellness Score"],subjective_wellness_score_avg = results_avg["Subjective Wellness Score"],physical_avg = results_avg["Physical Wellness Score"],social_avg = results_avg["Social Wellness Score"] ,emp_satifaction_avg = results_avg["Employee Satisfaction"],env_satisfaction_avg = results_avg["Environment Satisfaction"],work_life_balance_avg = results_avg["Work Life Balance"])
 
