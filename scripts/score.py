86a72b860269a594ab9547649cb5d6e069645243
aaditagarwal <agarwal.aadit99@gmail.com>
2021-04-22 01:36:06 +0530

    Results rounded to 3 decimals

--- scripts/score.py
+++ scripts/score.py
@@ -14,32 +14,32 @@ def score(data):
 
     #print(data)
     personal_keys = ['FRUITS_VEGGIES', 'DAILY_STRESS', 'BMI_RANGE', 'DONATION', 'PLACES_VISITED', 'CORE_CIRCLE', 'SUPPORTING_OTHERS', 'DAILY_STEPS','SOCIAL_NETWORK', 'SLEEP_HOURS', 'WEEKLY_MEDITATION', 'DAILY_SHOUTING', 'MaritalStatusID', 'Age_Group', 'Gender', 'RelationshipSatisfaction']
-    personal_score = (personal({key: data[key] for key in personal_keys}))*100/7
-    physical_score = (physical({key: data[key] for key in personal_keys}))*100/7
-    social_score = (social({key: data[key] for key in personal_keys}))*100/7
+    personal_score = round((personal({key: data[key] for key in personal_keys}))*100/7,3)
+    physical_score = round((physical({key: data[key] for key in personal_keys}))*100/7,3)
+    social_score = round((social({key: data[key] for key in personal_keys}))*100/7,3)
     
     emp_satisfaction_keys = ['Age_Group', 'PerfScoreID', 'SpecialProjectsCount', 'DaysLateLast30', 'Absences', 'Salary', 'EngagementSurvey', 'Gender', 'PercentSalaryHike', 'PerformanceRating', 'YearsAtCompany', 'YearsInCurrentRole', 'YearsSinceLastPromotion', 'YearsWithCurrManager', 'JobSatisfaction', 'StockOptionLevel', 'TrainingTimesLastYear', 'JobLevel', 'OverTime', 'NumCompaniesWorked']
-    emp_satisfaction_score = emp_satisfaction({key: data[key] for key in emp_satisfaction_keys})*100/5
+    emp_satisfaction_score = round(emp_satisfaction({key: data[key] for key in emp_satisfaction_keys})*100/5,3)
     
     env_satisfaction_keys = ['Age_Group', 'FromDiversityJobFairID', 'CitizenDesc', 'HispanicLatino', 'RaceDesc', 'Gender', 'Attrition', 'BusinessTravel', 'DistanceFromHome', 'JobInvolvement']
-    env_satisfaction_score = env_satisfaction({key: data[key] for key in env_satisfaction_keys})*100/4
+    env_satisfaction_score = round(env_satisfaction({key: data[key] for key in env_satisfaction_keys})*100/4,3)
     
     work_life_balance_keys = ['TODO_COMPLETED', 'LOST_VACATION', 'TIME_FOR_PASSION', 'FLOW', 'Age_Group', 'Gender']
-    work_life_balance_score = work_life_balance({key: data[key] for key in work_life_balance_keys})*100/4
+    work_life_balance_score = round(work_life_balance({key: data[key] for key in work_life_balance_keys})*100/4,3)
     
-    workplace_score = workplace(emp_satisfaction_score, env_satisfaction_score, work_life_balance_score)
+    workplace_score = round(workplace(emp_satisfaction_score, env_satisfaction_score, work_life_balance_score),3)
 
     subjective_keys = ['LIVE_VISION', 'ACHIEVEMENT', 'SUFFICIENT_INCOME', 'PERSONAL_AWARDS']
-    subjective_score = subjective({key: data[key] for key in subjective_keys})
+    subjective_score = round(subjective({key: data[key] for key in subjective_keys}),3)
 
     if personal_score == 0 or workplace_score == 0 or subjective_score == 0:
         gm = (math.log(personal_score+1) + math.log(workplace_score+1) + math.log(subjective_score+1)) / 3
         gm = math.pow(10,gm) - 1
         am = (personal_score + workplace_score + subjective_score) / 3
-        final_score = gm**2/am
+        final_score = round(gm**2/am,3)
     
     else:
-        final_score = statistics.harmonic_mean([personal_score, workplace_score, subjective_score])
+        final_score = round(statistics.harmonic_mean([personal_score, workplace_score, subjective_score]),3)
 
     return {"Employee Name": data['employee_name'],
             "Age": data['Age_Group'],
